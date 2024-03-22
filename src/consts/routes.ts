8f7ac0d7842d6fec060024fadb99a7705280b4f6
Alex Korzh <alexkorzh7@gmail.com>
2024-03-21 19:45:52 -0700

    Setup basic website layout

---
+++ src/consts/routes.ts
@@ -0,0 +1,39 @@
+const home = "/";
+const miscArticle = "/articles/:id";
+const life = "/life";
+const code = "/code";
+const guides = "/guides";
+const guidesArticle = "/guides/:id";
+const projects = "/projects";
+const games = "/games";
+const contact = "/contact";
+const login = "/login";
+const secret = {
+  home: "/secret",
+  life: "/secret/life",
+  code: "/secret/code",
+  guides: "/secret/guides",
+  projects: "/secret/projects",
+  games: "/secret/games",
+  editor: "/secret/editor",
+  editorMaterial: "/secret/editor/:id",
+  extMaterialEditor: "/secret/ext-material-editor",
+  extMaterialExistingMaterial: "/secret/ext-material-editor/:id",
+  guideEditor: "/secret/guide-editor",
+  guideEditorExistingMaterial: "/secret/guide-editor/:id",
+  fileUpload: "/secret/file-upload",
+};
+
+export default {
+  home,
+  miscArticle,
+  life,
+  code,
+  games,
+  guides,
+  guidesArticle,
+  projects,
+  contact,
+  login,
+  secret,
+};
