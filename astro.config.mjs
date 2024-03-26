62b9917cf1eadd8379c87b3bc0de55fa3611f3ff
Alex Korzh <alexkorzh7@gmail.com>
2024-03-26 11:54:19 -0700

    Clean up redirects and make all tag links to the first page

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,6 @@ import mdx from '@astrojs/mdx'
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx()],
-  redirects: {
-    '/tags/[tag]': '/tags/[tag]/1',
-  },
-  redirects: {
-    '/tags': '/tags/[tag]/1',
-  },
   markdown: {
     shikiConfig: {
       theme: 'night-owl',
